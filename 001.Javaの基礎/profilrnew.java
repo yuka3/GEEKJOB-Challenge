b6353b0c7169937697b7e0332edd2aa6d0379634
yuka3 <m4ga24@gmail.com>
2018-02-09 15:22:38 +0900

    Java基礎引数-戻り値2

---
+++ 001.Javaの基礎/profilrnew.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class profilrnew extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   void myprofile(PrintWriter pw) {
+       
+        //自己紹介メソッド
+    pw.print("私の名前は浦木由花<br>");
+    pw.print("平成3年3月4日生まれ<br>");
+    pw.print("趣味はスキンダイビング<br>"); 
+    
+     }
+   
+    boolean chackPlus(int num){
+    if (num>=0){return true;}
+    else{return false;}
+}
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+               
+   
+//人物のID、名前、生年月日、住所を配列にして返却するメソッド
+String[] id={"333<br>","yuka<br>","1991/03/04<br>","東京都世田谷区<br>"};
+out.print(id[1]);
+out.print(id[2]);
+out.print(id[3]);
+out.print(id[4]);
+
+
+//10回繰り返し
+    for(int i = 0; i < 10; i++){
+            myprofile(out);
+            if(true){
+            out.print("この処理は正しく実行できました<br>");
+            }else{
+             out.print("正しく実行できませんでした<br>");
+       
+      
+}
+}
+
+   
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
+
