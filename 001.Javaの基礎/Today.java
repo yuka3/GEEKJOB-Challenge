a627b704e70dc09c4368d6f6d3bef89b4a299f30
yuka3 <m4ga24@gmail.com>
2018-02-22 16:47:16 +0900

    Java基礎現在時刻のタイムスタンプの作成

---
+++ 001.Javaの基礎/Today.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+import java.util.Date;
+/**
+ *
+ * @author guest1Day
+ */
+public class Today {
+    public class today{
+    }
+       public static void main(String[] args) {
+        //現在時刻の設定と表示
+        Date d= new Date(1519284847L);
+        
+        System.out.println(d);
+    }
+       
+    
+    
+}
+
+ 
