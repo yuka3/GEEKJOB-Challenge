c79007aa0af8f42ca1e8309d51b089cd3beeda20
yuka3 <m4ga24@gmail.com>
2018-02-22 16:28:50 +0900

    Java基礎タイムスタンプの作成

---
+++ 001.Javaの基礎/Day.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+import java.util.Date;
+
+public class Day {
+    public static void main (String[] args){
+        // 日付情報を作成
+        Date someDay= new Date(1451574000);
+  
+       System.out.println(someDay);
+    }
+}
