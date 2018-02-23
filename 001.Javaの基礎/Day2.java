576c01fd74f6686296f9538804d4679eab4c1c21
yuka3 <m4ga24@gmail.com>
2018-02-23 14:29:15 +0900

    Java基礎タイムスタンプの表示4

---
+++ 001.Javaの基礎/Day2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Day2 {
+    public static void main( String[] args) {
+   
+        Date d =new Date (1420038000000L);
+        Date e =new Date (1451573999000L);
+        
+        //文字表示
+        System.out.print(1451573999000L-1420038000000L);
+    }
+
+}
