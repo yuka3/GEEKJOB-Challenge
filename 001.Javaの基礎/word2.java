765279b6c9b325ceb2ad95dc52a9d96797728ed3
yuka3 <m4ga24@gmail.com>
2018-02-27 13:08:30 +0900

    Java基礎 文字の入れ替え

---
+++ 001.Javaの基礎/word2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class word2 {
+    public static void main(String[] args){
+    
+    String word = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+    String word2 = word.replace("I","い");
+    String word3 = word2.replace("U","う");
+            
+     System.out.print(word3);
+        }
+}
