8e4820931a76eb1a9d1e3317eb297f7f27bdc055
yuka3 <m4ga24@gmail.com>
2018-02-27 11:28:03 +0900

    Java基礎 文字数の取得2

---
+++ 001.Javaの基礎/word1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class word1 {
+    public static void main(String[] args){
+    
+        String address = "123456@gmail.com";
+        String address2 = address.substring(6);
+        System.out.println(address2.length());
+    
+    }
+    
+}
