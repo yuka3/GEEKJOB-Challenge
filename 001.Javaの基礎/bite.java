4fac085dc2989f67d40e83290c1c831acfa2faaa
yuka3 <m4ga24@gmail.com>
2018-02-27 10:24:36 +0900

    Java基礎 バイト数と文字数の取得

---
+++ 001.Javaの基礎/bite.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class bite {
+    public static void main(String[] args){
+        
+    String word ="浦木由花";
+    
+    System.out.println(word.length());
+    System.out.println(word.getBytes().length);
+
+    }
+    
+    }
